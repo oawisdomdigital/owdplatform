a78923b50c354d8e49ca3b02804afa80ac1b10a9
oawisdomdigital <oawisdomdigitalfirm@gmail.com>
2024-12-17 20:55:29 +0100

    Initial commit

--- socialnetwork/settings.py
+++ socialnetwork/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)fd34l&bs_kp$j6k*2_rgab7cpo#23ik4zu(l&q(=l=8lqef52'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
